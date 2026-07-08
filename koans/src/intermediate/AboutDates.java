a64fb78b12b0a2b5e06e2420825a370661a9858e
Yasmine Saleh <yas2005usa@gmail.com>
2026-07-08 18:11:57 -0400

    completed 188 koans

--- koans/src/intermediate/AboutDates.java
+++ koans/src/intermediate/AboutDates.java
@@ -19,46 +19,46 @@ public class AboutDates {
 
     @Koan
     public void dateToString() {
-        assertEquals(date.toString(), __);
+        assertEquals(date.toString(), "1973-03-03T05:33:21");
     }
 
     @Koan
     public void changingDateValue() {
         date = date.plusHours(1);
-        assertEquals(date.toString(), __);
+        assertEquals(date.toString(), "1973-03-03T06:33:21");
     }
 
     @Koan
     public void usingPlusToChangeDatesDoesntWrapOtherFields() {
         date = date.plusMonths(12);
-        assertEquals(date.toString(), __);
+        assertEquals(date.toString(), "1974-03-03T06:33:21");
     }
 
     @Koan
     public void usingDateTimeFormatterToFormatDate() {
         String formattedDate = DateTimeFormatter.ISO_DATE.format(date);
-        assertEquals(formattedDate, __);
+        assertEquals(formattedDate, "1974-03-03");
     }
 
     @Koan
     public void usingDateTimeFormatterToFormatDateShort() {
         // Careful, formatted dates may contain non-breaking spaces!
         String formattedDate = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT).format(date);
-        assertEquals(formattedDate, __);
+        assertEquals(formattedDate, "3/3/74, 6:33 AM");
     }
 
     @Koan
     public void usingDateTimeFormatterToFormatDateFull() {
         String formattedDate = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).format(date);
         // There is also FormatStyle.LONG and FormatStyle.FULL... you get the idea ;-)
-        assertEquals(formattedDate, __);
+        assertEquals(formattedDate, "Mar 3, 1974, 6:33:21 AM");
     }
 
     @Koan
     public void usingDateTimeFormatterToParseDates() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
         LocalDate date2 = LocalDate.parse("01-01-2000", formatter);
-        assertEquals(date2.toString(), __);
+        assertEquals(date2.toString(), "2000-01-01");
         // What happened to the time? What do you need to change to keep the time as well?
     }
 }
--- koans/src/intermediate/AboutEquality.java
+++ koans/src/intermediate/AboutEquality.java
@@ -12,20 +12,20 @@ public class AboutEquality {
     public void sameObject() {
         Object a = new Object();
         Object b = a;
-        assertEquals(a == b, __);
+        assertEquals(a == b, true);
     }
 
     @Koan
     public void equalObject() {
         Integer a = new Integer(1);
         Integer b = new Integer(1);
-        assertEquals(a.equals(b), __);
-        assertEquals(b.equals(a), __);
+        assertEquals(a.equals(b), true);
+        assertEquals(b.equals(a), true);
     }
 
     @Koan
     public void noObjectShouldBeEqualToNull() {
-        assertEquals(new Object().equals(null), __);
+        assertEquals(new Object().equals(null), false);
     }
 
     static class Car {
