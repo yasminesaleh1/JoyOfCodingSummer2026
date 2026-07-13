438188f133946f53b1a4e7cb0c2067cc80d7abca
Yasmine Saleh <ysaleh@pdx.edu>
2026-07-12 17:02:35 -0700

    ran 'createFirstProject.sh'

---
+++ airline/src/main/java/edu/pdx/cs/joy/ysaleh/AirlineXmlHelper.java
@@ -0,0 +1,19 @@
+package edu.pdx.cs.joy.ysaleh;
+
+import edu.pdx.cs.joy.ProjectXmlHelper;
+
+/**
+ * This class provides easy access to the SYSTEM and PUBLIC ids for the
+ * Airline XML DTD and implements some convenient error handling methods.
+ */
+public class AirlineXmlHelper extends ProjectXmlHelper {
+
+  /** The Public ID for the Family Tree DTD */
+  protected static final String PUBLIC_ID =
+    "-//Joy of Coding at PSU//DTD Airline//EN";
+
+
+  public AirlineXmlHelper() {
+    super(PUBLIC_ID, "airline.dtd");
+  }
+}
---
+++ airline/src/test/java/edu/pdx/cs/joy/ysaleh/AirlineXmlHelperTest.java
@@ -0,0 +1,44 @@
+package edu.pdx.cs.joy.ysaleh;
+
+import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class AirlineXmlHelperTest {
+
+  @Test
+  void canParseValidXmlFile() throws ParserConfigurationException, IOException, SAXException {
+    DocumentBuilder builder = newValidatingDocumentBuilder(new AirlineXmlHelper());
+
+    builder.parse(this.getClass().getResourceAsStream("valid-airline.xml"));
+  }
+
+  @Test
+  void throwsExceptionWhenParsingInvalidXmlFile() throws ParserConfigurationException {
+    DocumentBuilder builder = newValidatingDocumentBuilder(new AirlineXmlHelper());
+
+    assertThrows(SAXParseException.class, () ->
+      builder.parse(this.getClass().getResourceAsStream("invalid-airline.xml"))
+    );
+  }
+
+  private static DocumentBuilder newValidatingDocumentBuilder(AirlineXmlHelper helper) throws ParserConfigurationException {
+    DocumentBuilderFactory factory =
+      DocumentBuilderFactory.newInstance();
+    factory.setValidating(true);
+
+    DocumentBuilder builder =
+      factory.newDocumentBuilder();
+    builder.setErrorHandler(helper);
+    builder.setEntityResolver(helper);
+    return builder;
+  }
+
+}
