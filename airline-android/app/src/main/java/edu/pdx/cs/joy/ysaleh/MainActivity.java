a47d26e5905d2f968cafa3500f2eb017c1a230c9
Yasmine Saleh <yas2005usa@gmail.com>
2026-08-13 18:07:28 -0700

    progress on project 5

--- airline-android/app/src/main/java/edu/pdx/cs/joy/ysaleh/MainActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs/joy/ysaleh/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.pdx.cs.joy.ysaleh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.background), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void createAirline(View view) {
+        Intent intent = new Intent(this, newAirlineActivity.class);
+        startActivity(intent);
+
+
+        // use for errors?
+        //Toast.makeText(this, "Error: ...", Toast.LENGTH_LONG).show();
+    }
+
+    public void newFlight(View view) {
+    }
+
+    public void viewAirlinePretty(View view) {
+    }
+
+    public void searchFlightsBetweenTwoAirports(View view) {
+    }
+
+    public void saveAirlineToFile(View view) {
     }
 }
